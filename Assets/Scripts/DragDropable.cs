cf38bca5d77133d1e6ebe1cd4c083d07424c82b9
Eskaven <skelothan@gmail.com>
2026-04-03 22:39:51 -0400

    Throwables can be flung by moving them quickly and letting go

--- Assets/Scripts/DragDropable.cs
+++ Assets/Scripts/DragDropable.cs
@@ -12,7 +12,8 @@ public class DragDropable : MonoBehaviour
 {
 	[SerializeField] private InputAction press, screenPos;
 
-	private float grabDistance = 0.5f;
+	private readonly float throwForceMultiplier = 0.35f;
+	private readonly float grabDistance = 0.5f;
 
 	private Vector3 curScreenPos;
 
@@ -27,6 +28,7 @@ public class DragDropable : MonoBehaviour
 			return (camera.ScreenToWorldPoint(curScreenPos + new Vector3(0, 0, z)));
 		}
 	}
+
 	private bool isClickedOn
 	{
 		get
@@ -40,6 +42,22 @@ public class DragDropable : MonoBehaviour
 			return false;
 		}
 	}
+
+	private Queue<Vector3> lastScreenPos = new Queue<Vector3>();
+	private readonly uint queueCapacity = 5;
+
+	private float getMouseSpeed()
+	{
+		int numItems = Mathf.Max(lastScreenPos.Count - 1, 1);
+		Vector3 sum = new Vector3();
+		while (lastScreenPos.Count > 1)
+		{
+			sum += lastScreenPos.Dequeue();
+		}
+
+		return Mathf.Abs((sum / numItems).magnitude);
+	}
+
 	private void Awake() 
 	{
 		camera = Camera.main;
@@ -48,28 +66,37 @@ public class DragDropable : MonoBehaviour
 		screenPos.performed += context => { curScreenPos = context.ReadValue<Vector2>(); };
 		press.performed += _ => { if(isClickedOn) StartCoroutine(Drag()); };
 		press.canceled += _ => { isDragging = false; };
-
 	}
 
 	private IEnumerator Drag()
 	{
+		Rigidbody rb = GetComponent<Rigidbody>();
 		isDragging = true;
 
 		// Move object towards camera when picked up
 		Vector3 cameraPos = camera.transform.position;
 		transform.position = (WorldPos + cameraPos) * grabDistance;
+
+		lastScreenPos = new Queue<Vector3>();
 		
 		// grab
-		GetComponent<Rigidbody>().useGravity = false;
+		rb.useGravity = false;
 		while(isDragging)
 		{
 			// dragging
 			transform.position = WorldPos;
-			Debug.Log("WorldPos: " + WorldPos.ToString());
+			yield return new WaitForFixedUpdate();
+			if (lastScreenPos.Count >= queueCapacity)
+				lastScreenPos.Dequeue();
+			lastScreenPos.Enqueue(curScreenPos);
 			yield return null;
 		}
 		// drop
-		GetComponent<Rigidbody>().useGravity = true;
+		yield return new WaitForFixedUpdate();
+		rb.useGravity = true;
+		Vector3 flingDirection = WorldPos - cameraPos;
+		Vector3 flingForce = flingDirection * (throwForceMultiplier * Mathf.Clamp(getMouseSpeed()/10, 0, 10));
+		rb.AddForce(flingForce, ForceMode.Impulse);
 
 
 	}
