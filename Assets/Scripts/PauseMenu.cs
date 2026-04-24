c01de83292131b995e330cc0f1b2d08045ba8a8d
Eskaven <skelothan@gmail.com>
2026-04-24 15:44:04 -0400

    Progression when all breakables destroyed

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -4,18 +4,51 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
 
+    KitchenCamera cameraScript;
+
     public GameObject pauseMenuButtons;
+
+    public GameObject nextButton;
+
+    public GameObject scene1Breakables;
+    public GameObject scene2Breakables;
+    public GameObject scene3Breakables;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Camera camera = Camera.main;
+        cameraScript = camera.GetComponent<KitchenCamera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        bool canMoveOn = true;
+        Break[] breakScripts;
+
+        switch (cameraScript.currentScene) {
+            case 0:
+                breakScripts = scene1Breakables.GetComponentsInChildren<Break>(true);
+                break;
+            case 1:
+                breakScripts = scene2Breakables.GetComponentsInChildren<Break>(true);
+                break;
+            case 2:
+                breakScripts = scene3Breakables.GetComponentsInChildren<Break>(true);
+                break;
+            default:
+                return;
+        }
+
+        for (int i = 0; i < breakScripts.Length; i++)
+        {
+            canMoveOn = canMoveOn && breakScripts[i].isBroken;
+        }
+
+        nextButton.SetActive(canMoveOn);
+
     }
 
     public void ShowPauseMenu()
@@ -46,9 +79,7 @@ public class PauseMenu : MonoBehaviour
 
     public void GoToNextScene()
     {
-        Camera camera = Camera.main;
-        KitchenCamera cameraScript = camera.GetComponent<KitchenCamera>();
-
         cameraScript.NextScene();
+        nextButton.SetActive(false);
     }
 }
